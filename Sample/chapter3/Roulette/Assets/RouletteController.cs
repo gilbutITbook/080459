8c2b48f626460080044310c713bf9e1d571cfd92
길벗 IT전문서/입문서 <6995518+gilbutITbook@users.noreply.github.com>
2025-10-14 14:09:35 +0900

    Add files via upload

---
+++ Sample/chapter3/Roulette/Assets/RouletteController.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.InputSystem;  // 입력을 감지하는 데 필요!
+
+public class RouletteController : MonoBehaviour
+{
+    float rotSpeed = 0;     // 회전 속도
+
+    void Start()
+    {
+        // 프레임레이트를 60으로 고정한다
+        Application.targetFrameRate = 60;
+    }
+
+    void Update()
+    {
+        // 마우스를 클릭하면 회전 속도를 설정한다
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            this.rotSpeed = 10;
+        }
+
+        // 회전 속도만큼 룰렛을 회전시킨다
+        transform.Rotate(0, 0, this.rotSpeed);
+
+        // 룰렛을 감속시킨다(추가)
+        this.rotSpeed *= 0.96f;
+    }
+}
